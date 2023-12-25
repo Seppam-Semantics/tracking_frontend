86d7c4383dc416027cbf0027f93ed71b74523791
krishnanBCG <gopibalu248@gmail.com>
2023-12-25 15:02:52 +0530

    commit

--- src/app/main/menu/garment-bundles/garment-bundles.component.ts
+++ src/app/main/menu/garment-bundles/garment-bundles.component.ts
@@ -9,93 +9,93 @@ import * as xls from 'xlsx'
 export class GarmentBundlesComponent {
 
   displayedColumns: string[] = ['WONo','WOLineno','FabBatchno','FabBarcode','RollNo','BundleBarcode','BundleNo',
-    'BundleRange','ActualCutPanelsPCS','ActualCutPanelsDATE','InputtosewPCS','InputtosewDATE','SewOutputPCS','SewOutputDATE',
-    'PackPCS','PackDATE','ReasonInputSew','ReasonSewPack'
-  ];
-  dataSource = ELEMENT_DATA;
-  users: any;
-  
-  readexcelfile(e:any){
+  'BundleRange','ActualCutPanelsPCS','ActualCutPanelsDATE','InputtosewPCS','InputtosewDATE','SewOutputPCS','SewOutputDATE',
+  'PackPCS','PackDATE','ReasonInputSew','ReasonSewPack'
+];
+dataSource = ELEMENT_DATA;
+users: any;
 
-    const file= e.target.files[0]
-    let fr = new FileReader();
+readexcelfile(e:any){
 
-    fr.readAsArrayBuffer(file);
+  const file= e.target.files[0]
+  let fr = new FileReader();
 
-    fr.onload= ()=>{
-      
-      let data = fr.result;
-          let workbook = xls.read(data,{type:'array'});
+  fr.readAsArrayBuffer(file);
 
-            const sheetname = workbook.SheetNames[0];
+  fr.onload= ()=>{
+    
+    let data = fr.result;
+        let workbook = xls.read(data,{type:'array'});
 
-            const sheet1 = workbook.Sheets[sheetname]
+          const sheetname = workbook.SheetNames[0];
 
-            this.users=xls.utils.sheet_to_json(sheet1,{raw:true});
-          
+          const sheet1 = workbook.Sheets[sheetname]
 
-            this.users.forEach((user: any) => {
-              this.dataSource=this.users
-              console.log(this.dataSource)
-              
-            });
-          };
+          this.users=xls.utils.sheet_to_json(sheet1,{raw:true});
+        
 
-        }
+          this.users.forEach((user: any) => {
+            this.dataSource=this.users
+            console.log(this.dataSource)
+            
+          });
+        };
+
+      }
 
 
 }
 
 export interface PeriodicElement {
 
-  WONo: any;
-  WOLineno: any;
-  FabBatchno: any;
-  FabBarcode: any;
-  RollNo: any;
-  BundleBarcode: any;
-  BundleNo: any;
-  BundleRange: any;
-  ActualCutPanelsPCS: any;
-  ActualCutPanelsDATE: any;
-  InputtosewPCS: any;
-  InputtosewDATE: any;
-  SewOutputPCS: any
-  SewOutputDATE: any;
-  PackPCS: any;
-  PackDATE: any;
-  ReasonInputSew: any;
-  ReasonSewPack: any;
+WONo: any;
+WOLineno: any;
+FabBatchno: any;
+FabBarcode: any;
+RollNo: any;
+BundleBarcode: any;
+BundleNo: any;
+BundleRange: any;
+ActualCutPanelsPCS: any;
+ActualCutPanelsDATE: any;
+InputtosewPCS: any;
+InputtosewDATE: any;
+SewOutputPCS: any
+SewOutputDATE: any;
+PackPCS: any;
+PackDATE: any;
+ReasonInputSew: any;
+ReasonSewPack: any;
 
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
 
-  {
+{
 
-    WONo: 1,
-    WOLineno: 2,
-    FabBatchno: 3,
-    FabBarcode: 4,
-    RollNo: 5,
-    BundleBarcode: 6,
-    BundleNo: 7,
-    BundleRange: 8,
+  WONo: 1,
+  WOLineno: 2,
+  FabBatchno: 3,
+  FabBarcode: 4,
+  RollNo: 5,
+  BundleBarcode: 6,
+  BundleNo: 7,
+  BundleRange: 8,
 
-    ActualCutPanelsPCS: 21,
-    ActualCutPanelsDATE: 23,
+  ActualCutPanelsPCS: 21,
+  ActualCutPanelsDATE: 23,
 
-    InputtosewPCS: 21,
-    InputtosewDATE: 34,
+  InputtosewPCS: 21,
+  InputtosewDATE: 34,
 
-    SewOutputPCS: 44,
-    SewOutputDATE: 45,
+  SewOutputPCS: 44,
+  SewOutputDATE: 45,
 
-    PackPCS: 12,
-    PackDATE: 21,
+  PackPCS: 12,
+  PackDATE: 21,
 
-    ReasonInputSew: 'ok',
-    ReasonSewPack: 'not ok'
-  }
+  ReasonInputSew: 'ok',
+  ReasonSewPack: 'not ok'
+}
 
 ];
--- src/app/main/menu/workorder-data/workorder-data.component.ts
+++ src/app/main/menu/workorder-data/workorder-data.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { SpeedDialItemModel } from '@syncfusion/ej2-angular-buttons';
 import * as xls from 'xlsx'
@@ -10,41 +11,69 @@ import * as xls from 'xlsx'
 })
 export class WorkorderDataComponent {
 
-  displayedColumns : string[] = [  'WOno', 'WOLineno', 'Order', 'Style', 'Color', 'Size', 'FabType',
-    'FabDia','FabGSM','Yarntype','YarnCount','KnitSL','SpinFty','KnitFty','DyeingFty','YarnLot','NoofRolls','PrintStatus' 
+  displayedColumns: string[] = ['WOno', 'WOLineno','buyer', 'orderNo', 'style', 'color', 'size', 'fabType',
+    'fabDia', 'fabGsm', 'yarnType', 'yarnCount', 'knitSL', 'spinFty', 'knitFty', 'dyeingFty', 'yarnLot', 'noRolls', 'PrintStatus'
   ];
 
-  dataSource: MatTableDataSource<PeriodicElement>=new MatTableDataSource<PeriodicElement>([])
+
+  workorder = new FormGroup({
+    WOno: new FormControl(''),
+    WOLineno: new FormControl(''),
+    Order: new FormControl(''),
+    Style: new FormControl(''),
+    Color: new FormControl(''),
+    Size: new FormControl(''),
+    FabType: new FormControl(''),
+    FabDia: new FormControl(''),
+    FabGSM: new FormControl(''),
+    Yarntype: new FormControl(''),
+    YarnCount: new FormControl(''),
+    KnitSL: new FormControl(''),
+    SpinFty: new FormControl(''),
+    KnitFty: new FormControl(''),
+    DyeingFty: new FormControl(''),
+    YarnLot: new FormControl(''),
+    NoofRolls: new FormControl(''),
+    PrintStatus: new FormControl(''),
+  })
+  workordersubmit() {
+    console.log(this.dataSource)
+  }
+
+  dataSource: MatTableDataSource<PeriodicElement> = new MatTableDataSource<PeriodicElement>([])
   users: any;
+  file: any;
 
-  readexcelfile(e:any){
+  readfile(e: any) {
+    this.file = e.target.files[0]
+  }
 
-    const file= e.target.files[0]
+
+  readexcelfile() {
+
     let fr = new FileReader();
 
-    fr.readAsArrayBuffer(file);
+    fr.readAsArrayBuffer(this.file);
 
-    fr.onload= ()=>{
-      
+    fr.onload = () => {
+
       let data = fr.result;
-          let workbook = xls.read(data,{type:'array'});
+      let workbook = xls.read(data, { type: 'array' });
 
-            const sheetname = workbook.SheetNames[0];
+      const sheetname = workbook.SheetNames[0];
 
-            const sheet1 = workbook.Sheets[sheetname]
+      const sheet1 = workbook.Sheets[sheetname]
 
-            this.users=xls.utils.sheet_to_json(sheet1,{raw:true});
-          
+      this.users = xls.utils.sheet_to_json(sheet1, { raw: true });
 
-            this.users.forEach((user: any) => {
-              this.dataSource=this.users
-              console.log(this.dataSource)
-              
-            });
-          };
 
-        }
+      this.users.forEach((user: any) => {
+        this.dataSource = this.users
+      });
+    };
+
   }
+}
 
 
 
@@ -52,23 +81,24 @@ export interface PeriodicElement {
 
   WOno: any;
   WOLineno: any;
-  Order:  any;
-  Style : any;
-  Color :any;
-  Size:any;
-  FabType:any;
-  FabDia:any;
-  FabGSM:any;
-  Yarntype:any;
-  YarnCount:any;
-  KnitSL:any;
-  SpinFty:any;
-  KnitFty:any;
-  DyeingFty:any;
-  YarnLot:any;
-  NoofRolls:any;
-  PrintStatus:any;
+  Order: any;
+  Style: any;
+  Color: any;
+  Size: any;
+  FabType: any;
+  FabDia: any;
+  FabGSM: any;
+  Yarntype: any;
+  YarnCount: any;
+  KnitSL: any;
+  SpinFty: any;
+  KnitFty: any;
+  DyeingFty: any;
+  YarnLot: any;
+  NoofRolls: any;
+  PrintStatus: any;
 }
+
 
 // const ELEMENT_DATA: PeriodicElement[] = [
 //   {
