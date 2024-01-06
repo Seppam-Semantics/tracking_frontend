d8dd870234af6a268130956b8c9864e93cee0eb1
krishnanBCG <gopibalu248@gmail.com>
2024-01-06 12:23:42 +0530

    commit

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -12,6 +12,7 @@ import { WelcomeComponent } from '../welcome/welcome.component';
 import { EditProfileComponent } from './masters/Roles and profiles/profiles/edit-profile/edit-profile.component';
 import { FabricRoll1Component } from './Transaction/Fabric Roll/fabric-roll-1/fabric-roll-1.component';
 
+
 import { BundleEntry1Component } from './Transaction/Bundle Entry/bundle-entry-1/bundle-entry-1.component';
 import { BundleEntry2Component } from './Transaction/Bundle Entry/bundle-entry-2/bundle-entry-2.component';
 import { BundleEntry3Component } from './Transaction/Bundle Entry/bundle-entry-3/bundle-entry-3.component';
@@ -20,6 +21,7 @@ import { AuthGuard } from '../auth.guard';
 const routes: Routes = [
   {path:'', component: AppComponent},
   
+
   { path: 'FabricRollData', component: FabricRollDataComponent},
   { path: 'WorkorderData', component: WorkorderDataComponent },
   { path: 'GarmentBundles' , component: GarmentBundlesComponent },
@@ -32,25 +34,11 @@ const routes: Routes = [
   { path: 'updateprofile' , component:EditProfileComponent},
   {path: 'viewemployees', component:ViewEmployeesComponent},
 
-<<<<<<< HEAD
   {path:'fabricroll1',component:FabricRoll1Component},
-  {path:'fabricroll2',component:FabricRoll2Component},
-  {path:'fabricroll3',component:FabricRoll3Component},
-  {path:'fabricroll4',component:FabricRoll4Component},
-  {path:'fabricroll5',component:FabricRoll5Component},
-  {path:'fabricroll6',component:FabricRoll6Component},
-  {path:'fabricroll7',component:FabricRoll7Component},
 
   {path:'bundleentry1',component:BundleEntry1Component},
   {path:'bundleentry2',component:BundleEntry2Component},
   {path:'bundleentry3',component:BundleEntry3Component},
-=======
-  {path:'fabricroll1',component:FabricRoll1Component,canActivate:[AuthGuard]},
-  
-  {path:'bundleentry1',component:BundleEntry1Component,canActivate:[AuthGuard]},
-  {path:'bundleentry2',component:BundleEntry2Component,canActivate:[AuthGuard]},
-  {path:'bundleentry3',component:BundleEntry3Component,canActivate:[AuthGuard]},
->>>>>>> ab7ecd5029d824b85f314b83741d143157450d6a
 ];
 
 @NgModule({
