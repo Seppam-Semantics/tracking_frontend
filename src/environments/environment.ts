a0be0c7e08e216aefc3fc8209f06badbc314a0f8
Seppam-Semantics <developer.seppam@gmail.com>
2024-05-07 15:38:42 +0530

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: true,
-    URL: 'https://tracker.seppam.com'
-    // URL:'http://localhost:2000'
+    // URL: 'https://tracker.seppam.com'
+    URL:'http://localhost:2000'
   };
   
   /*
