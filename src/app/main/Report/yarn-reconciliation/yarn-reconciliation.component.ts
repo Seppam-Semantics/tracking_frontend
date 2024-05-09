4b78d86cf2f4de8035f69c10fe0f205d44268ce1
krishnanBCG <gopibalu248@gmail.com>
2024-05-09 19:48:32 +0530

    commit

--- src/app/main/Report/yarn-reconciliation/yarn-reconciliation.component.ts
+++ src/app/main/Report/yarn-reconciliation/yarn-reconciliation.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
+
 
 @Component({
   selector: 'app-yarn-reconciliation',
@@ -9,18 +8,5 @@ import html2canvas from 'html2canvas';
 })
 export class YarnReconciliationComponent {
 
-  downloadAsPDF() {
-    const element: HTMLElement | null = document.getElementById('tableToConvert');
-    if (!element) {
-      console.error("Element with ID 'tableToConvert' not found");
-      return;
-    }
-    html2canvas(element).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jspdf.jsPDF();
-      const imgHeight = canvas.height * 208 / canvas.width;
-      pdf.addImage(imgData, 0, 0, 208, imgHeight);
-      pdf.save('table.pdf');
-    });
-  }
+
   }
